38cc0e3d01f9bb0663b63f126b5346d2f06b4c07
张辉程Huicheng Zhang <felixcheung97@gmail.com>
2022-06-12 21:15:47 +0200

    update

--- ms21_normalization.py
+++ ms21_normalization.py
@@ -17,13 +17,13 @@ output_path = sys.argv[2] #  '/media/felix/dataset/ms21_norm'
 # print(output_path)
 os.makedirs(output_path,exist_ok=True)
 # print(base_path)
-
-def loud_norm(path, output_path):
+fail_list = []
+def loud_norm(path, output_path, dir):
     data, rate = sf.read(path) # load audio
     # measure the loudness first 
     meter = pyln.Meter(rate) # create BS.1770 meter
     loudness = meter.integrated_loudness(data)
-    loudness_normalized_audio = pyln.normalize.loudness(data, loudness, -20.0) 
+    loudness_normalized_audio = pyln.normalize.loudness(data, loudness, -25.0) 
     sf.write(os.path.join(output_path,dir,file),loudness_normalized_audio, rate)
     print("Normalized ",path)
 
@@ -35,6 +35,11 @@ for root, dirs, files in os.walk(base_path):
             print(dir)
             os.makedirs(os.path.join(output_path,dir),exist_ok=True)
             try:
-                loud_norm(path, output_path)
+                loud_norm(path, output_path, dir)
             except:
                 print("something wrong", path)
+                fail_list.append(path)
+                
+with os.path.join(output_path,'fail_list.txt','w') as f0:
+    for i in fail_list:
+        f0.write(i)
