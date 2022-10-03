80976ad273886da8f1a259f0e83e53187f34f730
张辉程Huicheng Zhang <felixcheung97@gmail.com>
2022-10-03 10:34:05 +0200

    update

--- rule_based_mixing.py
+++ rule_based_mixing.py
@@ -35,6 +35,7 @@ def salient_info(X, nfft, threshold):
             magdB = 20*np.log10(sum(np.abs(X[0][:])))   # only calculate the first channel
         else:
             magdB = 20*np.log10(sum(np.abs(X[:,])))
+        print(np.min(magdB))
         mask = (magdB >= threshold)                 # same dimension as X
     return mask
 
@@ -89,8 +90,13 @@ def unit_assign(audio_list, threshold = -60):
             salient_reference = salient_info(X_ref, nfft, threshold)
             uniques_ref, counts_ref = np.unique(salient_reference, return_counts=True)
             count_ref = dict(zip(uniques_ref, counts_ref))
-            if count_ref[False] == len(salient_reference) :
-                print("Found Silent Reference Track")
+            if False in count_ref.keys():
+                if count_ref[False] == len(salient_reference) :
+                    print("Found Silent Reference Track")
+                    continue
+            else:
+                print("This track is salient all the time, probably contains leakage")
+                idp_idx.append(ref_idx)
                 continue
 
             salient_ref_frame_num = count_ref[True]
@@ -98,11 +104,15 @@ def unit_assign(audio_list, threshold = -60):
                 if can_idx in selected_idx:
                     continue
                 X = stft(audio_list[can_idx].T, nperseg=nfft )[-1]
-                salience = salient_info(X, nfft, threshold)
+                salience = salient_info(X, nfft, threshold)     # for those track that contain leakage, they can be all salient
                 uniques_can, counts_can = np.unique(salience, return_counts=True)
                 count_can = dict(zip(uniques_can, counts_can))
-                if count_can[False] == len(salience) :
-                    print("Found Silent Reference Track")
+                if False in count_can.keys():
+                    if count_can[False] == len(salience) :
+                        print("Found Silent Candidate Track")
+                        continue
+                else:
+                    print("This track is salient all the time, probably contains leakage")
                     continue
                 # only consider the salient part of the reference and current audio
                 denominator = max(count_can[True], salient_ref_frame_num)
