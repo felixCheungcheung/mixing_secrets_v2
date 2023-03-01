43db084ca7b0ce0fc729f371712ea50111339b4f
张辉程Huicheng Zhang <felixcheung97@gmail.com>
2023-03-01 09:55:28 +0800

    unrar_fixed

--- unrar.py
+++ unrar.py
@@ -10,14 +10,22 @@ if __name__ == "__main__":
     if not os.path.exists(multitrack_unzip_location):
         os.makedirs(multitrack_unzip_location)
 
+    error_info = []
     for i in os.listdir(multitrack_zip_location):
-        if i.split('.')[-1] == 'zip':
-            file_path = multitrack_zip_location+i
-            track_name = i.split('.')[:-1]
-            print(file_path)
-            with ZipFile(file_path, 'r') as zip:
-                # extracting all the files
-                print('Extracting all the files now...', )
-                zip.extractall(path=multitrack_unzip_location)
-                print('Done!')
-                exit()
+        try:
+            if i.split('.')[-1] == 'zip':
+                file_path = multitrack_zip_location+i
+                track_name = i.split('.')[:-1]
+                print(file_path)
+                with ZipFile(file_path, 'r') as zip:
+                    # extracting all the files
+                    print(f'Extracting all the files in {i}now...', )
+                    zip.extractall(path=multitrack_unzip_location)
+                    print(f'Sucessfully unzip {i}')
+        except Exception as error:
+                print(error)
+                error_info.append(i)
+                with open('failed_unzip_info.txt','w') as f0:
+                    for j in error_info:
+                        f0.write(j)
+                
